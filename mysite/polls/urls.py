9cd3f1d3e090db0fa0460fdaf5971d8e351ea43a
alexobinikpo <alex.obinikpo@yahoo.com>
2022-03-03 09:32:27 -0500

    Latest Features

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,8 +4,11 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
+    #path("", views.homepage, name="homepage"),
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
+    path("register", views.register_request, name="register"),
+    path("login", views.login_request, name="login")
 ]
